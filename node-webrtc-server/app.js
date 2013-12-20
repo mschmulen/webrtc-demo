a607a27f38f5590d05c4c252c4a33665135069c3
Matt Schmulen <matt.schmulen@gmail.com>
2013-12-19 22:39:56 -0800

    node webrtc server

---
+++ node-webrtc-server/app.js
@@ -0,0 +1,48 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+var webRTC = require('webrtc.io').listen(server);
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/style.css', function(req, res) {
+  res.sendfile(__dirname + '/public/style.css');
+});
+
+app.get('/script.js', function(req, res) {
+  res.sendfile(__dirname + '/public/script.js');
+});
+
+app.get('/webrtc.io.js', function(req, res) {
+  res.sendfile(__dirname + '/public/webrtc.io.js');
+});
+
+webRTC.rtc.on('chat_msg', function(data, socket) {
+  var roomList = webRTC.rtc.rooms[data.room] || [];
+
+  for (var i = 0; i < roomList.length; i++) {
+    var socketId = roomList[i];
+
+    if (socketId !== socket.id) {
+      var soc = webRTC.rtc.getSocket(socketId);
+
+      if (soc) {
+        soc.send(JSON.stringify({
+          "eventName": "receive_chat_msg",
+          "data": {
+            "messages": data.messages,
+            "color": data.color
+          }
+        }), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+      }
+    }
+  }
+});
